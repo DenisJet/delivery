ee7c56d5b2e9038f0b1e3f8d7bdb78b3e7bf4e51
Denis Kalkopf <ddenproton@protonmail.com>
2024-10-11 15:52:15 +0500

    add header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
+import { Inter } from 'next/font/google';
 import './globals.css';
+import Header from '@/components/header/header';
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Delivery',
@@ -25,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ru'>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
+      <head></head>
+      <body className={inter.className}>
+        <Header />
+        <main className='main'>{children}</main>
+      </body>
     </html>
   );
 }
---
+++ src/mockData.ts
@@ -0,0 +1,51 @@
+export const mockDataPlaces = [
+  {
+    id: 1,
+    name: 'Магнит',
+    image: 'https://loremflickr.com/320/240/',
+    address: 'Пролетарская 32',
+  },
+];
+
+export const mockDataCategories = [
+  {
+    id: 1,
+    name: 'Продукты',
+    image: 'https://loremflickr.com/320/240/',
+  },
+  {
+    id: 2,
+    name: 'Рестораны',
+    image: 'https://loremflickr.com/320/240/',
+  },
+  {
+    id: 3,
+    name: 'Аптеки',
+    image: 'https://loremflickr.com/320/240/',
+  },
+  {
+    id: 4,
+    name: 'Хоз. магазины',
+    image: 'https://loremflickr.com/320/240/',
+  },
+  {
+    id: 5,
+    name: 'Фаст фуд, шаурма',
+    image: 'https://loremflickr.com/320/240/',
+  },
+  {
+    id: 6,
+    name: 'Для детей',
+    image: 'https://loremflickr.com/320/240/',
+  },
+  {
+    id: 7,
+    name: 'Ремонт',
+    image: 'https://loremflickr.com/320/240/',
+  },
+  {
+    id: 8,
+    name: 'Разное',
+    image: 'https://loremflickr.com/320/240/',
+  },
+];
