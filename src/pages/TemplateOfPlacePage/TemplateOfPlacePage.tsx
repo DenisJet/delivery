9ef063d05a674e2c300581893d4539e6ab5e766b
Denis Kalkopf <ddenproton@protonmail.com>
2024-10-14 16:35:06 +0500

    fix

--- src/pages/TemplateOfPlacePage/TemplateOfPlacePage.tsx
+++ src/pages/TemplateOfPlacePage/TemplateOfPlacePage.tsx
@@ -3,22 +3,22 @@ import styles from './TemplateOfPlacePage.module.css';
 import { categories } from '@/mockData';
 
 export default function TemplateOfPlacePage({ place }: { place: IPlacePrice }) {
-  const subCategories = categories.find((category) => category.name === place.category)?.subCategories;
+  const subCategories = categories.find((category) => category.name === place?.category)?.subCategories;
 
   if (!place) return <div>Не удалось загрузить...</div>;
 
   return (
     <div className={styles.place}>
       <h2>{place.name}</h2>
-      {subCategories?.map((category) => {
-        const products = place.products?.filter((product) => product.category === category);
+      {subCategories?.map((subCategory) => {
+        const products = place.products?.filter((product) => product.category === subCategory);
 
         return (
-          <div className={styles.productList}>
+          <div className={styles.productList} key={subCategory}>
             {products &&
               products.map((product) => {
                 return (
-                  <div className={styles.productCard}>
+                  <div className={styles.productCard} key={product.name}>
                     <h3>{product.name}</h3>
                     <p>{product.price}</p>
                     <p>{product.quantity}</p>
