c0f7eb9c844a6a6a81189c1331237bfe6a8d65a2
Denis Kalkopf <ddenproton@protonmail.com>
2024-10-15 15:23:32 +0500

    add product buttons

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image';
 import styles from './ProductCard.module.css';
 import { IProduct } from '@/interfaces/place.interface';
+import { useState } from 'react';
 
 export default function ProductCard({ product }: { product: IProduct }) {
+  const [counter, setCounter] = useState(0);
   const DEFAULT_IMAGE_URL = '/images/placeholder.png';
 
   return (
@@ -17,9 +19,19 @@ export default function ProductCard({ product }: { product: IProduct }) {
       <p className={styles.productPrice}>{product.price} p</p>
       <p className={styles.productName}>{product.name}</p>
       <p className={styles.productQuantity}>{product.quantity}</p>
-      <button className={styles.addButton} type='button'>
-        +
-      </button>
+      <div className={styles.productButtons}>
+        {counter > 0 && (
+          <>
+            <button onClick={() => setCounter(counter - 1)} className={styles.removeButton} type='button'>
+              -
+            </button>
+            <div>{counter}</div>{' '}
+          </>
+        )}
+        <button onClick={() => setCounter(counter + 1)} className={styles.addButton} type='button'>
+          +
+        </button>
+      </div>
     </div>
   );
 }
