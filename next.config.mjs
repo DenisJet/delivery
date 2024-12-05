6dbe3437cf21cdd309b9d8cd64b661130fbfdfc5
Denis Kalkopf <ddenproton@protonmail.com>
2024-12-05 15:37:07 +0500

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,7 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'loremflickr.com',
-        port: '',
-        pathname: '/320/240/**',
-      },
-    ],
+    remotePatterns: [],
   },
 };
 
